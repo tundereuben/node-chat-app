255c8b6b69b1e8b5c0956ef137b0921a0ef75770
Tunde <tundeogunjimi@gmail.com>
2018-09-17 15:18:27 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Andre',
-    text: 'Yup, tha works for me.'
-  })
 });
 
 socket.on('disconnect', function () {
