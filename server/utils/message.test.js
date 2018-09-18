890ad57e35399a4f170636b3b074998948fb7565
Tunde <tundeogunjimi@gmail.com>
2018-09-18 14:43:30 +0100

    Add geolocation support via geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,7 @@
 var expect = require('expect');
 
 var {generateMessage} = require('./message');
+var {generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -9,5 +10,18 @@ describe('generateMessage', () => {
     expect(message.from).toBe('Tunde');
     expect(message.text).toBe('Test message');
     expect(message.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Lucent';
+    var latitude = 1;
+    var longitude = 1;
+    var message = generateLocationMessage(from, latitude, longitude);
+    var url = message.url;
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
   });
 });
